03dd0ab9b31746410c17f2a9cd6d564b2f28b27e
Ryan Nagel <RNagel29@gmail.com>
2022-09-20 19:20:13 -0600

    Add Game class

---
+++ game.js
@@ -0,0 +1,39 @@
+class Game {
+  constructor(human, computer, type) {
+    this.human = human;
+    this.computer = computer;
+    this.type = type;
+    this.classicGameResults = {
+      rock: {
+        rock: 'Draw!',
+        paper: 'Computer Wins!',
+        scissors: 'Human Wins!',
+      },
+      paper: {
+        rock: 'Human Wins!',
+        paper: 'Draw!',
+        scissors: 'Computer Wins!',
+      },
+      scissors: {
+        rock: 'Computer Wins!',
+        paper: 'Human Wins!',
+        scissors: 'Draw!',
+      }
+    }
+    executeGame() {
+      var result = this.classicGameResults[this.human.fighter][this.computer.fighter];
+      if (result === 'Human Wins!') {
+        this.human.wins++;
+      }
+      if (result === 'Computer Wins!') {
+        this.computer.wins++;
+      }
+      resetGame();
+      return result;
+    }
+  }
+}
+
+//clicking the type of game will kick off the game. Which means it should create a new instance of Game.
+// will put an event listener on the button section. Based on which button is clicked, it will pass through a game type.
+//Defaults right now to human and computer, but can change later if i'd like to.
--- player.js
+++ player.js
@@ -1,10 +1,14 @@
 class Player {
   constructor(name, token) {
     this.name = name;
-    this token = token;
+    this.token = token;
     this.wins = 0;
+
   }
-  takeTurn() {
-    
+  takeTurn(fighter) {
+    this.fighter = fighter;
+    computerTakeTurn();
   }
 }
+
+//will need event listener for the fighters. Will trigger the takeTurn event handler.
