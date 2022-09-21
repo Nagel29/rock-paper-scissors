30249c22b24b6de815937134fc40240e2a39adeb
Ryan Nagel <RNagel29@gmail.com>
2022-09-20 21:04:25 -0600

    Gets classic game fighters to display

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 var humanWinsDisplay = document.querySelector('#human-wins');
 var computerWinsDisplay = document.querySelector('#computer-wins');
+var gameSelectionSection = document.querySelector('#game-selection-section');
+var buttonContainer = document.querySelector('#button-container');
 var classicGameButton = document.querySelector('#classic-game-button');
 var advancedGameButton = document.querySelector('#advanced-game-button');
+var battleSection = document.querySelector('#battle-section');
 var rockButton = document.querySelector('#rock-button');
 var paperButton = document.querySelector('#paper-button');
 var scissorsButton = document.querySelector('#scissors-button');
@@ -9,8 +12,21 @@ var computer = new Player('Computer', '🤖');
 var classicGameFighters = ['rock', 'paper', 'scissors'];
 
 //this will need to later go into an event listener/handler. This will be for classic game. Will need another for advanced.
-var human = new Player('Human', '👴');
-var game = new Game(human, computer, 'Classic Game');
+
+
+
+buttonContainer.addEventListener('click', startGame);
+
+
+function startGame(event) {
+  var human = new Player('Human', '👴');
+  if (event.target === classicGameButton) {
+    var game = new Game(human, computer, 'Classic Game');
+    gameSelectionSection.classList.add('hidden');
+    battleSection.classList.remove('hidden');
+ }
+
+}
 
 function computerTakeTurn() {
   if (game.type === 'Classic Game') {
